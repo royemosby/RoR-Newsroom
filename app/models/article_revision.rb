d53cb4040909275030505616402b805714861493
Roy Mosby <ego.madking@gmail.com>
2021-04-23 18:36:20 -0400

    corrects model association from "User" to "Employee"

--- app/models/article_revision.rb
+++ app/models/article_revision.rb
@@ -1,4 +1,4 @@
 class ArticleRevision < ApplicationRecord
   belongs_to :article
-  belongs_to :reviewer, foreign_key: :reviewer_id, class_name: "User"
+  belongs_to :reviewer, foreign_key: :reviewer_id, class_name: "Employee"
 end

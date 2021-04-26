244a5fb9a848bec5aa542dfe582e11cfe58b1e0f
Roy Mosby <ego.madking@gmail.com>
2021-04-26 07:41:41 -0400

    adds association to EditorRevision, ArticleUpdate

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,5 +3,6 @@ class Employee < ApplicationRecord
   has_many :employee_roles
   has_many :roles, through: :employee_roles
   has_many :articles
-  has_many :article_revisions, foreign_key: :reviewer_id
+  has_many :editor_revisions, foreign_key: :reviewer_id
+  has_many :article_updates, foreign_key: :updater_id
 end

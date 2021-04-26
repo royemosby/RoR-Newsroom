2911bca7263ce2c1dc14714ecd7deb40865c81d5
Roy Mosby <ego.madking@gmail.com>
2021-04-26 17:13:53 -0400

    adds validation for role presence and uniqueness

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,7 @@
 class Role < ApplicationRecord
   has_many :employee_roles
   has_many :employees, through: :employee_roles
+
+  validates :role, presence: true
+  validates :role, uniqueness: true
 end

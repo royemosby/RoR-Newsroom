d6d8ce4c49a96488c8fe9c1eb2e2f3d01b22ca9e
Roy Mosby <ego.madking@gmail.com>
2021-04-26 09:18:10 -0400

    adds foreign key to ArticleUpdate association

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
   belongs_to :employee, optional: true
   has_many :editor_revisions
   has_many :reviewers, through: :editor_revisions, class_name: "Employee"
-  has_many :article_updates
+  has_many :article_updates, foreign_key: :article_id
   has_many :updaters, through: :article_updates, class_name: "Employee"
   has_many :article_tags
   has_many :tags, through: :article_tags

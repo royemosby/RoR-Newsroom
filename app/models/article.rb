baf79cd57c94b571b5e9961fae5f861cd1b95817
Roy Mosby <ego.madking@gmail.com>
2021-04-26 07:40:00 -0400

    creates associations in Article for EditorRevisions, Employees as reviewers, ArticleUpdates, Employees as updaters, ArticleTags, and Tags

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ApplicationRecord
   belongs_to :employee, optional: true
-  has_many :article_revisions
-  
+  has_many :editor_revisions
+  has_many :reviewers, through: :editor_revisions, class_name: "Employee"
+  has_many :article_updates
+  has_many :updaters, through: :article_updates, class_name: "Employee"
+  has_many :article_tags
+  has_many :tags, through: :article_tags
 end

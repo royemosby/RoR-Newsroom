1c5e210b5d33b65b14bd867b109d9912d55e8d52
Roy Mosby <ego.madking@gmail.com>
2021-04-26 17:14:39 -0400

    adds validations for tag presence, uniqueness, and cannot contain spaces

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,17 @@
 class Tag < ApplicationRecord
   has_many :article_tags
   has_many :articles, through: :article_tags
+  validates :tag, presence: true
+  validates :tag, uniqueness: true
+  validate :cannot_contain_spaces
+
+  def cannot_contain_spaces
+    if tag.present?
+      if tag.index(/\s/)
+        errors.add(:tag, "Tags must be single words and cannot contain spaces.")
+      end
+    else
+      errors.add(:tag, "Cannot create an empy tag")
+    end
+  end
 end

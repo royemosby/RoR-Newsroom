e2931cc99dffcb0a2e4a8d3ae9920784ff9f8264
Roy Mosby <ego.madking@gmail.com>
2021-04-26 07:43:07 -0400

    Renames model call, adds roles to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,9 @@ Article.create!(title: "this one is assigned", employee_id: journalist.id)
 
 reviewed = Article.create!(title: "this one is assigned and has a review", employee_id: journalist.id)
 
-ArticleRevision.create!(content: "Please update article title per meeting", reviewer_id: editor.id, article_id: reviewed.id)
+EditorRevision.create!(content: "Please update article title per meeting", reviewer_id: editor.id, article_id: reviewed.id)
+
+roles = ["create", "approve", "assign", "edit", "review", "publish"]
+
+#TODO roles need to be pre-populated into production database
+roles.each { |r| Role.create!(role: r)}

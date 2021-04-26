3134e369364c3b74b1a69b340c1b7c46c5ff154b
Roy Mosby <ego.madking@gmail.com>
2021-04-26 17:13:34 -0400

    adds validations for first and last name presence

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,4 +5,7 @@ class Employee < ApplicationRecord
   has_many :articles
   has_many :editor_revisions, foreign_key: :reviewer_id
   has_many :article_updates, foreign_key: :updater_id
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end

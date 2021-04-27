a5ec4f13ce22015861f657ba657b992f94c567c2
Roy Mosby <ego.madking@gmail.com>
2021-04-26 21:21:30 -0400

    creates ArticleUpdates controller

--- app/controllers/article_updates_controller.rb
+++ app/controllers/article_updates_controller.rb
@@ -1,2 +1,9 @@
 class ArticleUpdatesController < ApplicationController
+
+  # TODO consider order ArticleUpdates by recency
+  # TODO does ArticleUpdates need an isolated form?
+
+  def index
+    @article_updates = ArticleUpdate.all
+  end
 end

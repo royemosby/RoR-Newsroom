40214ac0eb70262a2efeb95a32377e24ebcae4b5
Roy Mosby <ego.madking@gmail.com>
2021-04-26 21:23:09 -0400

    adds initial routes to models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :article_updates
+  resources :articles
+  resources :editor_revisions
+  resources :employees
+  resources :roles, except: [:show]
+  resources :tags, only: [:index, :destroy]
+  root to: "articles#index"
 end

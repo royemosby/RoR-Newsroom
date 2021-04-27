07a259233ea40ea37b08263b501d5cb0884f0863
Roy Mosby <ego.madking@gmail.com>
2021-04-26 20:38:33 -0400

    adds action upon group to Article model tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,9 +5,6 @@ RSpec.describe Article, type: :model do
     before(:each) do
       DatabaseCleaner.clean
     end
-  end
-
-  describe "ACTIONS UPON" do
     it "title length at least 3 characters" do
       @too_short_title = Article.create(title: "no", content: "This is a 10 word sentence so will pass validation.")
       expect(@too_short_title.valid?).to be false
@@ -16,12 +13,29 @@ RSpec.describe Article, type: :model do
       @too_short_content = Article.create(title: "Short of content", content: "This is a 9 word sentence which will fail.")
       expect(@too_short_content.valid?).to be false
     end
-    xit "can be edited" do
+  end
+
+  describe "ACTIONS UPON" do
+    before(:all) do
+      DatabaseCleaner.clean
+      @article = Article.create(title: "long enough", content: "This is a 10 word sentence so will pass validation.")
+      ["rails", "community"].each { |i| Tag.create(tag: i)}
+      @tags = Tag.all
+    end
+    it "can have a tag added" do
+      @article.tags << @tags.first
+      expect(@article.tags.length).to be 1
+    end
+
+    it "can have several tags added" do
+      @article.tags << @tags.second
+      expect(@article.tags.length).to be 2
+    end
+
+    xit "each tag is unique" do
 
     end
-    xit "can have tags added" do
 
-    end
     xit "can have editor_revisions appended" do
 
     end

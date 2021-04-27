6b51a77d34261dfcef8ad243c5c95b2f60a406e5
Roy Mosby <ego.madking@gmail.com>
2021-04-27 10:46:01 -0400

    adds login route

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   resources :roles, except: [:show]
   resources :tags, only: [:index, :destroy]
   root to: "articles#index"
+  get "/login", to: "sessions#new", as: "login"
 end

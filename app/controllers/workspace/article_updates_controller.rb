357cc5cd141de3811ab110a4f8be7913b6b06b0c
Roy Mosby <ego.madking@gmail.com>
2021-04-28 15:55:04 -0400

    adds show action and 2 helper methods

--- app/controllers/workspace/article_updates_controller.rb
+++ app/controllers/workspace/article_updates_controller.rb
@@ -1,9 +1,21 @@
 class Workspace::ArticleUpdatesController < ApplicationController
 
+  before_action :find_update, only: [:show, :edit, :delete]
   # TODO consider order ArticleUpdates by recency
   # TODO does ArticleUpdates need an isolated form?
 
   def index
     @article_updates = ArticleUpdate.all
+  end
+
+  def show
+  end
+  
+  def find_update
+    @article_update = ArticleUpdate.find(params[:id])
+  end
+
+  def update_params
+    params.require(:article_update).permit(:content, :article_id)
   end
 end

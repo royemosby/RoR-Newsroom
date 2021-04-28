7be88023bf1d5bee82adc34530684b5c7c1f9884
Roy Mosby <ego.madking@gmail.com>
2021-04-28 15:55:38 -0400

    adds actions: index and destroy. Adds params helper.

--- app/controllers/workspace/tags_controller.rb
+++ app/controllers/workspace/tags_controller.rb
@@ -1,2 +1,16 @@
 class Workspace::TagsController < ApplicationController
+
+  #TODO verify logon before deleting tags
+  def index
+    @tags = Tag.all
+  end
+
+  def destroy
+    tag = Tag.find(id: params[:id])
+    tag.destroy
+  end
+
+  def tag_params
+    params.require(:tag).permit(name)
+  end
 end

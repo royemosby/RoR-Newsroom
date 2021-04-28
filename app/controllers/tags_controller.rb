518ecd31a65db5273d86ad2636a66eb5abe8f500
Roy Mosby <ego.madking@gmail.com>
2021-04-28 13:20:14 -0400

    creates tags controller and assets

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,11 @@
 class TagsController < ApplicationController
+
   def index
     @tags = Tag.all
+  end
+
+  #TODO build out Tag show to include associated articles
+  def show
+    @tag = Tag.find_by(id: params[:id])
   end
 end

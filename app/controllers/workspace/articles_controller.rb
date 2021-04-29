c84ad761c447790237538e7aeafb7630f60d02f2
Roy Mosby <ego.madking@gmail.com>
2021-04-28 21:14:59 -0400

    stubs out new, create, edit and, update actions. Adds helper methods

--- app/controllers/workspace/articles_controller.rb
+++ app/controllers/workspace/articles_controller.rb
@@ -2,6 +2,9 @@ class Workspace::ArticlesController < ApplicationController
 
   #TODO Article actions: new, create, edit, update, destroy
   #TODO How to handle article publishing view?
+  #TODO Add in tags (csv with find_or_create_by)
+  #TODO If article has editor_revisions, show them
+  before_action :find_article, only: [:show, :edit, :update]
 
   def index
     @articles = Article.all
@@ -10,4 +13,40 @@ class Workspace::ArticlesController < ApplicationController
   def show
     @article = Article.find_by(id: params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  #TODO autoassign for journalists (has "edit", "assign" only to self auths)
+  #TODO author assignment dropdown for "create" auth
+  def create
+    @article = Article.create(article_params)
+    if @article.save
+      redirect_to workspace_article_path(@article)
+    else
+      render :edit, alert: "This article was not saved."
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+    if @article.save
+      redirect_to workspace_article_path(@article)
+    else
+      render :edit, alert: "Changes were not saved. See errors below."
+    end
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content, :status)
+  end
+
 end

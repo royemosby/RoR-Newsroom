bc580829fdebac9000d85ba74af0fa3749fc1a46
Roy Mosby <ego.madking@gmail.com>
2021-04-30 23:04:54 -0400

    fleshes out seeds for entire model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,75 @@
-#users
-
+#employees
+sr_editor = Employee.create!(first_name: "deborah", last_name: "smith", title: "Senior Editor", password: "password")
 editor = Employee.create!(first_name: "roy", last_name: "mosby", title: "chief editor", password: "password")
 journalist = Employee.create!(first_name: "samantha", last_name: "croix", title: "journalist at large", password: "password")
+journalist_2 = Employee.create!(first_name: "levon", last_name: "johnson", title: "journalist at large", password: "password")
 intern = Employee.create!(first_name: "larry", last_name: "grotch", title: "newsroom intern", password: "password")
 
-#articles
+#content prep for articles
+def fake_content
+  Faker::Lorem.paragraphs(number: 5).join
+end
+def fake_title
+  Faker::Company.bs
+end
 
-Article.create!(title: "This one's a floater", content: Faker::Lorem.paragraphs(number: 3).join)
-Article.create!(title: "this one is assigned", employee_id: journalist.id, content: Faker::Lorem.paragraphs(number: 3).join)
+#articles: new
+Article.create!(title: "This one is unassigned", content: fake_content)
+Article.create!(title: "this one is assigned", employee_id: journalist.id, content: fake_content)
+Article.create!(title: "this one is also assigned", employee_id: journalist_2.id, content: fake_content)
 
-#editor review
+#articles: approved
+Article.create!(title: "Article approved #1", employee_id: journalist_2.id, content: fake_content, status: "approved")
+Article.create!(title: "Article approved #2", employee_id: journalist_2.id, content: fake_content, status: "approved")
+Article.create!(title: "Article approved but unassigned", employee_id: journalist_2.id, content: fake_content, status: "approved")
 
-reviewed = Article.create!(title: "this one is assigned and has a review", employee_id: journalist.id)
+#articles: review
+Article.create!(title: "Article for review #1", employee_id: journalist.id, content: fake_content, status: "review")
+Article.create!(title: "Article for review #2", employee_id: journalist_2.id, content: fake_content, status: "review")
+
+# articles: published
+10.times do |i| 
+  j = [journalist, journalist_2]
+  Article.create!(title: fake_title, employee: j[i%2], content: fake_content, status: "published" )
+end
+
+#editor_revision, article status edit
+reviewed = Article.create!(title: "Article has review", employee_id: journalist.id, content: fake_content, status: "edit")
 EditorRevision.create!(content: "Please update article title per meeting", reviewer_id: editor.id, article_id: reviewed.id)
 
-#article update
+#article_update
 
-has_update = Article.create(title: "this one has an update", employee_id: journalist.id)
+has_update = Article.create!(title: "Published article. Has update", content: fake_content, employee_id: journalist.id, status: "published")
 ArticleUpdate.create!(content: "Nostrum did not lorem that day.", article_id: has_update.id, updater_id: intern.id)
 
 
 #roles
-roles = ["create", "approve", "assign", "edit", "review", "publish"]
+roles = ["create", "self_create", "approve", "assign", "self_assign", "edit", "review", "publish", "update"]
 
 #TODO roles need to be pre-populated into production database
 roles.each { |r| Role.create!(role: r)}
 
+#employee_roles
+create = Role.find_by(role: "create")
+self_create = Role.find_by(role: "self_create")
+approve = Role.find_by(role: "approve")
+assign = Role.find_by(role: "assign")
+self_assign = Role.find_by(role: "self_assign")
+edit = Role.find_by(role: "edit")
+review = Role.find_by(role: "review")
+publish = Role.find_by(role: "publish")
+update = Role.find_by(role: "update")
+
+sr_editor.roles << [create, approve, self_assign, edit, review, publish, update]
+editor.roles << [approve, edit, review, publish, update]
+journalist.roles << [self_create, self_assign, edit, update]
+journalist_2.roles << [self_create, self_assign, edit, update]
+intern.roles << [create, assign, update]
+
 #tags
+tags = ["rails", "ruby", "gem", "hotfix", "internet", "ux", "community", "templating", "library", "feature"]
+tags.each { |t| Tag.create!(tag: t)}
+
+#article_tags
+tagged_article = Article.create!(title: "Tagged article", content: fake_content, employee_id: journalist_2.id, status: "published")
+tagged_article.tags << [Tag.first, Tag.second, Tag.third]

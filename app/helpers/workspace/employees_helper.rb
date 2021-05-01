8d992136b79d3ac5bea821f7315458eb2f8ba1f2
Roy Mosby <ego.madking@gmail.com>
2021-04-30 23:11:19 -0400

    adds full_name helper for employees

--- app/helpers/workspace/employees_helper.rb
+++ app/helpers/workspace/employees_helper.rb
@@ -1,2 +1,15 @@
 module Workspace::EmployeesHelper
+
+  def full_name(employee)
+    if employee.first_name.empty?
+      unless employee.gh_name.empty?
+        employee.gh_name
+      else
+        "This person has no name"
+      end
+    else
+      "#{employee.first_name} #{employee.last_name}"
+    end
+  end
+  
 end

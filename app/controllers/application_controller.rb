cdd3bfb0351eaed9a0375759fa663dce298d7ae3
Roy Mosby <ego.madking@gmail.com>
2021-05-01 19:18:46 -0400

    adds filter methods for workspace::controllers to ensure logged on

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  add_flash_types :success
+  
+  def current_user
+    @employee = Employee.find_by(id: session[:employee_id])
+  end
+
+  def logged_on
+    unless current_user
+      redirect_to "/", alert: "You have to be logged in to access that page."
+    end
+  end
 end

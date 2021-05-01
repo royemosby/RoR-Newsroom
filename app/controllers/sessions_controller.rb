70c56ef2162cbafcaf5f538b25ff2e5f550fc078
Roy Mosby <ego.madking@gmail.com>
2021-04-30 23:06:56 -0400

    #edits comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,17 @@ class SessionsController < ApplicationController
     end
   end
 
+  #TODO create username/password option
+  #strong params for create action
   def create
+  
     @employee = Employee.find_or_create_by(gh_uid: auth['uid']) do |e|
       e.gh_name = auth['info']['name']
       e.gh_email = auth['info']['email']
-      e.password = ENV['OAUTH_USER_PASS'] #TODO balance has_secure_password w/omniauth requirement
+      e.password = ENV['OAUTH_USER_PASS'] #TODO change to secure_random
     end
     session[:employee_id] = @employee.id
-    if @employee.first_name.nil? || @employee.last_name.nil
+    if @employee.first_name.nil? || @employee.last_name.nil?
       redirect_to edit_workspace_employee_path(@employee), alert: "Please complete your account registration form"
     else
       redirect_to workspace_employee_path(@employee), alert: "Welcome, #{@employee.first_name}"

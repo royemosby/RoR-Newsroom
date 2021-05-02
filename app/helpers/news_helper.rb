26d3da49ba09356ec6decaa8d44165d04f25379a
Roy Mosby <ego.madking@gmail.com>
2021-05-02 12:33:42 -0400

    adds date_published helper for news

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -1,2 +1,10 @@
 module NewsHelper
+
+  def date_published(article)
+    if article.published_date.nil?
+      "This article has not been published yet."
+    else
+      article.published_date.strftime("Published %F")
+    end
+  end
 end

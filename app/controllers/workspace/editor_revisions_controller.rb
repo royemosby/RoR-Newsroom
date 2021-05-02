ffd96169daacf00ff60788900b54166a1f4e92ff
Roy Mosby <ego.madking@gmail.com>
2021-05-02 18:18:11 -0400

    creates actions show, new, create. Adds strong params

--- app/controllers/workspace/editor_revisions_controller.rb
+++ app/controllers/workspace/editor_revisions_controller.rb
@@ -10,4 +10,29 @@ class Workspace::EditorRevisionsController < ApplicationController
       @editor_revisions = EditorRevision.all
     end
   end
+
+  def show
+  end
+
+  def new
+    if params[:article_id]
+      @editor_revision = EditorRevision.new
+      @editor_revision.article = Article.find_by(id: params[:article_id])
+    else
+      redirect_to workspace_employee_path(session[:employee_id]), alert: "Editor revisions cannot be made without being associated with an article."
+    end
+  end
+
+  def create
+    @editor_revision = EditorRevision.create(revision_params)
+    if @editor_revision.save
+      redirect_to workspace_article_path(@editor_revision[:article_id]), success: "Editor revision posted successfully."
+    else
+      render new, alert: "Form did not save successfully."
+    end
+  end
+
+  def revision_params
+    params.require(:editor_revision).permit(:content, :article_id, :reviewer_id)
+  end
 end

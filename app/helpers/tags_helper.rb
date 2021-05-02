77f7b0d8c4d2039b529f6375422720c2c680aa84
Roy Mosby <ego.madking@gmail.com>
2021-05-02 12:34:02 -0400

    adds tag list helper for tags

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,2 +1,13 @@
 module TagsHelper
+
+  def news_tag_list(article)
+    if article.tags.empty?
+      "<span class='is-italic'>This article does not have any tags</span>".html_safe
+    else
+      article.tags.collect do |t|
+        link_to "#{t.tag}", tag_path(t), class: "button is-link is-small ml-10", style: "pointer-events: none; margin-right: 5px;"
+      end.join.html_safe
+    end
+
+  end
 end

ed56d340528dc356a817982df2fd36e97dab8c61
Roy Mosby <ego.madking@gmail.com>
2021-05-02 16:52:43 -0400

    displays employee name or unassigned depending on presence of employee_id

--- app/helpers/workspace/articles_helper.rb
+++ app/helpers/workspace/articles_helper.rb
@@ -17,6 +17,14 @@ module Workspace::ArticlesHelper
     pluralize(((word_count(article).to_f)/250).ceil, "minute")
   end
 
+  def assigned_to(article)
+    if article.employee.nil?
+      "no one"
+    else
+      link_to article.employee.first_name, workspace_employee_path(article.employee_id)
+    end
+  end
+
   # TODO get_tags is an untested method!!
   def get_tags(article)
     unless article.tags.empty?

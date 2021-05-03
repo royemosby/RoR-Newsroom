b0377cd85a3edef6dab0c5b002b81729eb652427
Roy Mosby <ego.madking@gmail.com>
2021-05-02 21:50:24 -0400

    removes unused method

--- app/controllers/workspace/tags_controller.rb
+++ app/controllers/workspace/tags_controller.rb
@@ -2,15 +2,8 @@ class Workspace::TagsController < ApplicationController
 
   before_action :logged_on
 
-  #TODO delete buttons for tags
-  #TODO show usage stats
   def index
     @tags = Tag.all
-  end
-
-  def destroy
-    tag = Tag.find(id: params[:id])
-    tag.destroy
   end
 
   def tag_params

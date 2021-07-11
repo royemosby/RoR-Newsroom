2b9e7d502be25227a0d7e1b9fc9ab7f734a91321
Roy Mosby <ego.madking@gmail.com>
2021-07-11 13:44:48 -0400

    removes conditional

--- app/helpers/workspace/employees_helper.rb
+++ app/helpers/workspace/employees_helper.rb
@@ -2,11 +2,7 @@ module Workspace::EmployeesHelper
 
   def full_name(employee)
 
-    if !!employee.first_name ||employee.first_name.nil? || employee.first_name.empty?
-      "This person has no set no first or last name"
-    else
       "#{employee.first_name} #{employee.last_name}"
-    end
   end
   
 end
